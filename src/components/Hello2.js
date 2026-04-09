c7b268e82e16dcf150b5449d9d1e49b0bb39bf89
luckyillia <izaichenko@technikumlodz.pl>
2026-04-09 14:22:17 +0200

    First commit

---
+++ src/components/Hello2.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+class Hello2 extends Component{
+    render(){
+        return(
+            <h1>Hello World 2</h1>
+        );
+    }
+}
+export default Hello2;
