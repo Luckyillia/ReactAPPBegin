3f4b039b9328f371f2fa7d4e582a8222ce3ee8f7
luckyillia <izaichenko@technikumlodz.pl>
2026-04-23 14:12:17 +0200

    Route

--- src/App.js
+++ src/App.js
@@ -1,78 +1,97 @@
 import './App.css';
 import './css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-routerdom"; 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Hello from './components/Hello';
 import Hello2 from './components/Hello2';
-
 import Button from './components/Button';
 import Button2 from './components/Button2';
-
-import Product from './components/Product'
-import Counter from './components/Counter'
-import Counter2 from './components/Counter2'
-import TodoApp from './components/TodoApp'
-
+import Product from './components/Product';
+import Counter from './components/Counter';
+import Counter2 from './components/Counter2';
+import TodoApp from './components/TodoApp';
 import Header from './components/Header';
 
-const product = { 
-  name: "Laptop", 
-  price: 3500, 
-  description: "Wydajny laptop do pracy i nauki" 
-}; 
+const product = {
+  name: "Laptop",
+  price: 3500,
+  description: "Wydajny laptop do pracy i nauki"
+};
+
+
+
+function Home() {
+  return (
+    <div className="container text-center">
+      <div className="row justify-content-center">
+        <Hello />
+        <Hello2 />
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-sm d-flex justify-content-center">
+          <Button name="btn1" label="Click me" bgColor="success" />
+        </div>
+        <div className="col-sm d-flex justify-content-center">
+          <Button name="btn1" label="Click me" bgColor="danger" />
+        </div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-sm d-flex justify-content-center">
+          <Button2 name="btn1" label="Click me" bgColor="info" />
+        </div>
+        <div className="col-sm d-flex justify-content-center">
+          <Button2 name="btn1" label="Click me" bgColor="warning" />
+        </div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-sm mx-auto"><Counter /></div>
+        <div className="col-sm mx-auto"><Counter2 /></div>
+      </div>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div className="container text-center">
+      <div className="mx-auto">
+        <Product
+          name={product.name}
+          price={product.price}
+          description={product.description}
+          srcImg="https://th.bing.com/th/id/OIP.TABlGYoUA-adhs4_vvSAsAHaE8"
+        />
+      </div>
+    </div>
+  );
+}
+
+function Todo() {
+  return (
+    <div className="container text-center">
+      <div className="mx-auto"><TodoApp /></div>
+    </div>
+  );
+}
+
+function NotFound() { 
+  return <h2>404 - Strona nie znaleziona</h2>; 
+}
 
 function App() {
   return (
     <div className="App">
-      <Router> 
-        <Header /> 
-        <section className="content"> 
-          <Routes> 
-            <Route path="/" element={<Home />} /> 
-            <Route path="about" element={<About />} /> 
-          </Routes> 
-        </section> 
+      <Router>
+        <Header />
+        <section className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/todo" element={<Todo />} />
+             <Route path="*" element={<NotFound />} />
+          </Routes>
+        </section>
       </Router>
-      <div className="container text-center">
-        <div className="row justify-content-center">
-          <Hello/>
-          <Hello2/>
-        </div>
-        <div className="row justify-content-center">
-          <div className="col-sm d-flex justify-content-center">
-            <Button name="btn1" label="Click me" bgColor="success"/> 
-          </div>
-          <div className="col-sm d-flex justify-content-center">
-            <Button name="btn1" label="Click me" bgColor="danger"/>
-          </div>
-        </div>
-        <div className="row justify-content-center">
-          <div className="col-sm d-flex justify-content-center">
-            <Button2 name="btn1" label="Click me" bgColor="info"/>
-          </div>
-          <div className="col-sm d-flex justify-content-center">
-            <Button2 name="btn1" label="Click me" bgColor="warning"/>
-          </div>
-        </div>
-        <div className="row justify-content-center">
-          <div className="col-sm mx-auto">
-            <Product name={product.name} price={product.price} description={product.description} srcImg="https://th.bing.com/th/id/OIP.TABlGYoUA-adhs4_vvSAsAHaE8?o=7rm=3&rs=1&pid=ImgDetMain&o=7&rm=3" />
-          </div>
-        </div>
-        <div className="row justify-content-center">
-          <div className="col-sm mx-auto">
-            <Counter />
-          </div>
-          <div className="col-sm mx-auto">
-            <Counter2 />
-          </div>
-        </div>
-        <div className="row justify-content-center">
-          <div className="col-sm mx-auto">
-            <TodoApp />
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,32 @@
-import React, { Component } from 'react'; 
-import { BrowserRouter as Router, Routes, Route, Link } from "react-routerdom"; 
-class Header extends React.Component { 
-    render() { 
-        return ( 
-            <header> 
-                <nav className='nav'> 
-                    <ul className="main-nav"> 
-                        <li className="nav-item"><Link to="/">Home</Link></li> 
-                        <li className="nav-item"><Link to="/about">About</Link></li> 
-                    </ul> 
-                </nav> 
-            </header> 
-        ); 
-    } 
-} 
+import React from 'react';
+import { Link } from "react-router-dom";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="navbar-brand"><Link to="/">Home</Link></div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <div className="navbar-link"><Link to="/">Home</Link></div>
+                </li>
+                <li class="nav-item">
+                    <div className="navbar-link"><Link to="/about">About</Link></div>
+                </li>
+                <li class="nav-item">
+                    <div className="navbar-link"><Link to="/todo">Todo</Link></div>
+                </li>
+                </ul>
+            </div>
+        </nav>
+      </header>
+    );
+  }
+}
+
 export default Header;
